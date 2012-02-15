bf14d491687c0bb5609e11d0e0f97dc32d36fd12
Ben Morrow <ben@morrow.me.uk>
2012-02-15 08:21:51 +0000

    Inflators.
    I'm not sure this is the best implementation. While classes seem nice,
    it's a lot of overhead for what is (in the end) a single function, and
    it makes the question of loading app-specific inflators a little tricky.
    (What are they called? How do we find them? Having to say "+Prospect::
    DB::Inflate::PartialDate" all the time would be infuriating, but OTOH
    I've so far avoided making assumptions about the user's package
    structure beyond everything being under the DB.) It's possible it would
    be better just to maintain a hash of subrefs in Inflate.pm, with a
    number of inflators pre-registered.
    
    Currently only inflators are supported, not deflators: I'm not sure what
    the best way to support deflators would be. Obviously they would need to
    be invoked in $Self and $Arg, but it's not clear to me how to detect the
    things that need deflating.
    
    One (obvious) solution would be to only support them in $Self, and only
    for columns registered as having an inflator. This is so limited I
    suspect it's useless: it means, among other things, that you can't pass
    a value from one Row to a method on another without deflating it
    manually. The inconsistency would be infuriating.
    
    The ideal solution would look at all values returned from placeholder{},
    and if they were deflatable objects, deflate them. The tricky part is
    identifying deflatable objects, and matching them to deflators. I
    believe the only correct answer would be to check every object against
    every registered deflator with ->isa, which would be horrendously slow.
    Even that doesn't tell us what to do if an object ->isa two classes with
    different deflators.
    
    One possible answer would be, *if* an object uses UNIVERSAL::isa (which
    will be usual), to get the linearized ISA and check it against our
    registrations. The fallback case will still be slow, but uncommon. Of
    course, this is still a whole lot of checking for objects which don't
    deflate at all.
    
    Another, of course, would be to wrap inflated objects in a proxy which
    knows how to deflate them again. This would simplify things enormously,
    at the cost of not allowing the user to pass objects in from the
    outside. YA would be to require queries to specify their deflated
    arguments explicitly, though I suspect that would get boring very fast.

---
+++ lib/DBIx/Irian/Inflate.pm
@@ -0,0 +1,8 @@
+package DBIx::Irian::Inflate;
+
+use Carp;
+
+sub inflate { croak "Unimplemented" }
+sub deflate { croak "Unimplemented" }
+
+1;
--- lib/DBIx/Irian/Row.pm
+++ lib/DBIx/Irian/Row.pm
@@ -6,7 +6,7 @@ use strict;
 use parent "DBIx::Irian::QuerySet";
 
 use DBIx::Irian   undef, qw(
-    install_sub register lookup find_sym load_class
+    install_sub register lookup find_sym load_class qualify
 );
 
 no overloading;
@@ -16,14 +16,25 @@ use overload
 
 sub _DB { $_[0][0] }
 
-sub _new { bless [$_[1], $_[2]], $_[0] }
+sub _new {
+    my ($class, $db, $row) = @_;
+    if (my $inf = lookup $class, "inflate") {
+        $inf->[$_] and $row->[$_] = $inf->[$_]->inflate($row->[$_])
+            for 0..$#$row;
+    }
+    bless [$db, $row], $class;
+}
 
 our %SUGAR = (
     columns => sub {
         my $pkg = caller;
+
+        register $pkg, mycols => [ @_ ];
+
         my $parents = lookup($pkg, "extends") || [];
         my @inherit = map @{ lookup($_, "cols") || [] }, @$parents;
         register $pkg, cols => [ @inherit, @_ ];
+
         for my $ix (0..$#_) {
             install_sub $pkg, $_[$ix], sub { $_[0][1][$ix + @inherit] };
         }
@@ -41,6 +52,42 @@ our %SUGAR = (
 
         local $" = "][";
         warn "EXTENDS: [$pkg] [@ps]\n";
+    },
+
+    inflate => sub {
+        my (%inf) = @_;
+        my $pkg = caller;
+
+        local $" = "][";
+        warn "INFLATE [$pkg]: [@_]\n";
+
+        my $mycols = lookup $pkg, "mycols" or Carp::croak 
+            "'inflate' must come after 'columns'";
+        my @inf;
+
+        my $parents = lookup($pkg, "extends")   || [];
+        for (@$parents) {
+            my $cols = lookup($_, "cols")       || [];
+            my $inf  = lookup($_, "inflate")    || [];
+
+            # make sure we get enough entries to cover all the columns
+            push @inf, @$inf[0..$#$cols];
+        }
+
+        for (@$mycols) {
+            my $inf = $inf{$_};
+            if ($inf) {
+                $inf = qualify $inf, "DBIx::Irian::Inflate";
+
+                eval "require $inf; 1;" or die $@;
+                $inf->isa("DBIx::Irian::Inflate") or Carp::croak
+                    "'$inf' is not an Inflate class";
+            }
+
+            push @inf, $inf;
+        }
+
+        register $pkg, inflate => \@inf;
     },
 
 );
