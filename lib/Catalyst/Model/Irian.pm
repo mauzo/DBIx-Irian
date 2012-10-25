303eb694c375fe3c30dc7e1fcffe40acadabbdbc
Ben Morrow <ben@morrow.me.uk>
2012-10-25 06:02:57 +0100

    Docs for the Cat model and :via(Logger).

--- lib/Catalyst/Model/Irian.pm
+++ lib/Catalyst/Model/Irian.pm
@@ -1,5 +1,11 @@
 package Catalyst::Model::Irian;
 
+=head1 NAME
+
+Catalyst::Model::Irian - Use an Irian DB as a Catalyst Model
+
+=cut
+
 use 5.010;
 use Moose;
 use Catalyst::Utils;
@@ -8,6 +14,68 @@ use PerlIO::via::Logger;
 our $VERSION = "1";
 
 extends "Catalyst::Model";
+
+=head1 SYNOPSIS
+
+    package MyApp::Model::DB;
+
+    use Moose;
+    extends "Catalyst::Model::Irian";
+
+    __PACKAGE__->config(
+        DB      => "MyApp::DB",
+        dsn     => "dbi:...",
+    );
+
+=head1 DESCRIPTION
+
+This is a L<Catalyst|Catalyst> L<Model|Catalyst::Model> for connecting
+to an L<Irian|DBIx::Irian> database. It overrides the C<COMPONENT>
+method, so C<< $c->model >> will return an L<Irian::DB|DBIx::Irian::DB>
+object rather than one derived from Catalyst::Model.
+
+=head2 Configuration
+
+The following keys can be passed to C<< __PACKAGE__->config >> or
+specified in your app's config file.
+
+=over 4
+
+=item C<DB>
+
+The name of the DBIx::Irian::DB class to instantiate. Required.
+
+=item C<redirect_trace>
+
+If this is set to C<1>, both Irian and DBI will have their logging
+redirected through the Catalyst logger. By default logging will be at
+the C<debug> level; if this is set to a string, that method will be
+called on the log object instead.
+
+This redirection is done when the model is instantiated, and has global
+effect. All Irian and all DBI logging in the program will be affected.
+
+=item C<dsn>
+
+=item C<user>
+
+=item C<password>
+
+=item C<mode>
+
+=item C<dbi>
+
+=item C<dbc>
+
+=item C<driver>
+
+These will be passed through to L<C<< DB->new >>|DBIx::Irian::DB/new>.
+At least C<dsn> is required. Be aware that C<dbc> and C<driver> need an
+object, so they cannot be specified in a config file.
+
+=back
+
+=cut
 
 sub COMPONENT {
     my ($self, $app, $args) = @_;
@@ -44,3 +112,18 @@ sub COMPONENT {
 }
 
 1;
+
+=head1 SEE ALSO
+
+See L<DBIx::Irian> for bug reporting and other general information.
+
+L<Catalyst>, L<Catalyst::Model>, L<Catalyst::Model::Adaptor>.
+
+L<DBI> log redirection is implemented using L<PerlIO::via::Logger>.
+
+=head1 COPYRIGHT
+
+Copyright 2012 Ben Morrow.
+
+Released under the 2-clause BSD licence.
+
--- lib/PerlIO/via/Logger.pm
+++ lib/PerlIO/via/Logger.pm
@@ -1,8 +1,83 @@
 package PerlIO::via::Logger;
 
+=head1 NAME
+
+PerlIO::via::Logger - Redirect IO to a logger object
+
+=cut
+
 use 5.010;
 use warnings;
 use strict;
+
+=head1 SYNOPSIS
+
+    open my $LOG, ">:via(Logger)", {
+        logger  => $logger,
+        level   => "debug",
+        prefix  => "DBI",
+    };
+
+    DBI->trace(DBI->trace, $LOG);
+
+=head1 DESCRIPTION
+
+This is a PerlIO layer which redirects all IO to a log object. As the
+synopsis shows, it is intended for redirecting L<DBI>'s trace output,
+but it's general enough to be useful in other situations.
+
+To create a filehandle C<$FH> which logs messages by calling the
+C<debug> method of a logging object C<$log>, use 3-arg C<open> like
+this:
+
+    open my $FH, ">:via(Logger)", {
+        logger  => $log,
+        level   => "debug",
+    };
+
+The filehandle should be opened for writing; it may be opened
+read/write, but reads will always return EOF. Do not attempt to push
+this layer onto an existing filehandle with C<binmode>, since there is
+no way to pass in the configuration. Other layers (C<:encoding>, say)
+may be pushed on top, but since this layer will never pass data down
+there is no point including layers underneath.
+
+Configuration is via a hashref passed to C<open> in place of a filename.
+The following keys are recognised.
+
+=over 4
+
+=item C<logger>
+
+The log object to use. Required.
+
+=item C<level>
+
+A string naming the method to call on the log object for each log entry.
+Required.
+
+=item C<prefix>
+
+A string to prefix to each log entry before passing it off to the
+logger. If this string is non-empty a colon and a space will be inserted
+between the prefix and the message. Optional, defaults to the empty
+string.
+
+=item C<rs>
+
+The string separating log entries. Since the upward interface is a
+filehandle, it's likely that calling code may split a single entry
+across several writes, or write several entries at once. This allows us
+to separate them out again to feed to the log object. 
+
+If this is the empty string, each call to C<WRITE> will be written as a
+single log entry.
+
+Optional, defaults to C<"\n">.
+
+=back
+
+=cut
 
 sub PUSHED {
     my ($class) = @_;
@@ -43,3 +118,19 @@ sub WRITE {
 sub CLOSE { 0 }
 
 1;
+
+=head1 AUTHOR
+
+Ben Morrow <ben@morrow.me.uk>
+
+=head1 BUGS
+
+Currently this module is distributed as part of L<DBIx::Irian>, so
+please report bugs to <bug-DBIx-Irian@rt.cpan.org>.
+
+=head1 COPYRIGHT
+
+Copyright 2012 Ben Morrow.
+
+Released under the 2-clause BSD license.
+
